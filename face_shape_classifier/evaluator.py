01042dceec39a4629f9b4d73daec27d5ce6db7bd
Hans-0101 <hyhsumin@naver.com>
2022-01-14 00:04:36 +0900

    feat:changing evaluate fun

--- face_shape_classifier/evaluator.py
+++ face_shape_classifier/evaluator.py
@@ -3,13 +3,12 @@ import numpy as np
 from keras.preprocessing import image
 
 class FaceShapeEvaluator():
-    def __init__(self, model_path, test_data) -> None:
+    def __init__(self, model_path) -> None:
         self.model_path = model_path
-        self.test_data = test_data
         self.model = tf.keras.models.load_model(self.model_path)
 
-    def evluate(self):
-        test_image = image.load_img(self.test_data, target_size = (200,200))
+    def evluate(self, test_data):
+        test_image = image.load_img(test_data, target_size = (200,200))
         #test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
         result = self.model.predict(test_image)
